dc2a9623301efb97ff157eb0dfbe7191fc9fe97c
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-06-17 16:38:45 -0400

    CRUD ventas

--- pos_project/ventas/urls.py
+++ pos_project/ventas/urls.py
@@ -6,5 +6,7 @@ app_name = 'ventas'
 urlpatterns = [
     path('nueva/', views.nueva_venta, name='nueva'),
     path('detalle/<int:id>/', views.detalle_venta, name='detalle'),
-
+    path('pdf/<int:id>/', views.venta_pdf, name='pdf'),
+    path('csv/<int:id>/', views.venta_csv, name='csv'),
+    path('', views.historial_ventas, name='historial'),
 ]
