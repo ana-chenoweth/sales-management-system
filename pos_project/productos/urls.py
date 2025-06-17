d0989d56072f495d23168e0dd90a6310f976ece7
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-06-17 11:49:23 -0400

    vistas y URLs productos

---
+++ pos_project/productos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'productos'
+
+urlpatterns = [
+    path('', views.producto_listar, name='listar'),
+    path('crear/', views.producto_crear, name='crear'),
+    path('editar/<int:id>/', views.producto_editar, name='editar'),
+    path('eliminar/<int:id>/', views.producto_eliminar, name='eliminar'),
+]
--- pos_project/productos/views.py
+++ pos_project/productos/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Producto
+from .forms import ProductoForm
 
-# Create your views here.
+def producto_listar(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/listar.html', {'productos': productos})
+
+def producto_crear(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('productos:listar')
+    else:
+        form = ProductoForm()
+    return render(request, 'productos/formulario.html', {'form': form, 'accion': 'Crear'})
+
+def producto_editar(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('productos:listar')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'productos/formulario.html', {'form': form, 'accion': 'Editar'})
+
+def producto_eliminar(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    return redirect('productos:listar')
